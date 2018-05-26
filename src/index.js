08f4b1b69bbed722a18049fc87690cd81839f497
강재훈 <kangjaehoon@gangjaehun-ui-MacBook-Pro.local>
2018-05-27 00:54:40 +0900

    indexPage + sliders added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
+import axios from "axios";
+
+const todoAPI = axios.create({
+  baseURL: process.env.API_URL
 });
+
+const rootEl = document.querySelector(".root");
+
+function login(token) {
+  localStorage.setItem("token", token);
+  todoAPI.defaults.headers["Authorization"] = `Bearer ${token}`;
+}
+
+function logout() {
+  localStorage.removeItem("token");
+  delete todoAPI.defaults.headers["Authorization"];
+}
+
+const templates = {
+  skillMain: document.querySelector("#skill-main").content
+};
+
+function render(frag) {
+  rootEl.textContent = "";
+  rootEl.appendChild(frag);
+}
+
+
+async function indexPage(){
+
+  const skillFrag = document.importNode(templates.skillMain, true);
+
+  $(document).ready(function() {
+
+    // main slider
+    setInterval(function () {      
+        var sliderOne = $('.slider-wrapper__slider1');
+        var sliderTwo = $('.slider-wrapper__slider2');
+        
+        if(sliderOne.css('margin-left') == '0px') {
+            sliderTwo.css({'margin-left' : '100%'});
+            sliderTwo.animate({marginLeft : '0px'}, 'slow');
+            sliderOne.animate({marginLeft : '-100%'}, 'slow');
+        } else {
+            sliderOne.css({'margin-left' : '100%'});
+            sliderOne.animate({marginLeft : '0px'}, 'slow');
+            sliderTwo.animate({marginLeft : '-100%'}, 'slow');
+        }
+    }, 7000);
+});
+
+  render(skillFrag);
+}
+
+indexPage();
